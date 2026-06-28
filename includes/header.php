65644e13ef2a9bb27a4f7a76ae0ef85735d74777
ard4nnn <ard4nnn@gmail.com>
2026-06-28 19:38:50 +0800

    menambahkan parameter cache-buster untuk stylesheet style.css

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/init.php';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo $base_url; ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo $base_url; ?>assets/css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
 
